612a5d3d34c5b6eb0fe32511415c1e4a2e941966
Ahnaf-Tahmid-009 <59658472+Ahnaf-Tahmid-009@users.noreply.github.com>
2020-01-08 08:10:23 -0800

    The first commit

---
+++ Addition.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Addition
+{
+	public static void main(String args[])
+	{	int x=10,y=25;
+		System.out.println("Addition of two integers :");
+		//int sum = x+y;
+		System.out.println("Summation : "+ (x+y));
+	}
+}
